cac06786f9ad1b8a0f6751c560f0d24c4adb305a
djm43@students.ptcollege.edu <>
2018-11-12 15:48:53 -0500

    Do the thing

---
+++ src/github/midterm/GitHubMidterm.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package github.midterm;
+
+/**
+ *
+ * @author domin
+ */
+public class GitHubMidterm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //You said to make it simple Cottrell,
+        //this is as simple as it gets
+    }
+    
+}
